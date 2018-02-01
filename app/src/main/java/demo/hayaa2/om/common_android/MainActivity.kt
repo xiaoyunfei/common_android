24b9146a85899ab3a92b61ce2f65ac886dbf3bed
xiaoyunfei <xiaoyunfeigeili@163.com>
2018-02-01 16:48:01 +0800

    first commit

---
+++ app/src/main/java/demo/hayaa2/om/common_android/MainActivity.kt
@@ -0,0 +1,12 @@
+package demo.hayaa2.om.common_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
